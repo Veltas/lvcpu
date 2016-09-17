6f364a612672a0ecfed598322a8c3a555a908681
Christopher Leonard <chris.j.leonard@gmx.com>
2016-09-17 03:19:06 +0100

    asm: Initial Source_data commit

---
+++ asm/source_data.cpp
@@ -0,0 +1,123 @@
+#include "source_data.hpp"
+
+#include <list>
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <regex>
+
+namespace fs = std::experimental::filesystem;
+
+std::vector<Source_data::Source_line> Source_data::load_file(
+	const fs::path &file_path)
+{
+	// Avoid nesting
+	if (
+		std::find(_source_paths.begin(), _source_paths.end(), file_path)
+		!= _source_paths.end()
+	) {
+		return {};
+	} else {
+		_source_paths.emplace_back(file_path);
+	}
+	const auto *const file_p = &_source_paths.back();
+	// Open file
+	std::ifstream in_file(file_path.native());
+	if (!in_file.is_open()) {
+		throw std::runtime_error{"Unable to load " + file_path.native()};
+	}
+	// Collect lines
+	std::vector<Source_line> result;
+	while (in_file) {
+		std::string current_line;
+		std::getline(in_file, current_line);
+		const std::regex include_regex(
+			"^\\s*\\.include\\s*\"([^\"]+)\"\\s*(?:;.*|)$"
+		);
+		std::smatch include_match;
+		// Handle .include lines recursively
+		if (std::regex_match(current_line, include_match, include_regex)) {
+			const std::string filename(
+				include_match[1].first,
+				include_match[1].second
+			);
+			auto included_source = load_file(
+				fs::absolute(file_path.parent_path() / filename)
+			);
+			result.reserve(result.size() + included_source.size());
+			for (auto &included_line: included_source) {
+				result.emplace_back(std::move(included_line));
+			}
+		} else {
+			result.emplace_back(file_p, std::move(current_line));
+		}
+	}
+	return result;
+}
+
+Source_data::Source_data(const std::string &root_filename) :
+	_lines(load_file(fs::current_path() / root_filename))
+{}
+
+namespace {
+	void source_range_check(const std::size_t index, const std::size_t size)
+	{
+		if (index >= size)
+			throw std::out_of_range{"Source_data logic error, range check failed"};
+	}
+
+	std::string get_substring(
+		const std::string &str,
+		const std::size_t i1)
+	{
+		source_range_check(i1, str.size());
+		return std::string(str.begin() + i1, str.end());
+	}
+
+	std::string get_substring(
+		const std::string &str,
+		const std::size_t i1,
+		const std::size_t i2)
+	{
+		source_range_check(i1, str.size());
+		source_range_check(i2, str.size());
+		return std::string(str.begin() + i1, str.begin() + i2 + 1);
+	}
+}
+
+std::string Source_data::get_range(const Range &range) const
+{
+	const auto &start = range.start, &end = range.end;
+	if (
+		start.line_n > end.line_n ||
+		(start.line_n == end.line_n && start.col_n > end.col_n)
+	) {
+		return std::string();
+	} else if (start.line_n == end.line_n) {
+		const auto &line = _lines.at(start.line_n).second;
+		return get_substring(line, start.col_n, end.col_n);
+	} else {
+		std::string result;
+		result += get_substring(_lines.at(start.line_n).second, start.col_n);
+		for (std::size_t i = 1; i < end.line_n - start.line_n; ++i) {
+			result += '\n' + _lines.at(start.line_n).second;
+		}
+		result += '\n' + get_substring(_lines.at(end.line_n).second, 0, end.col_n);
+		return std::move(result);
+	}
+}
+
+char Source_data::get_char(const Position &pos) const
+{
+	return _lines.at(pos.line_n).second.at(pos.col_n);
+}
+
+const std::string & Source_data::get_line(const std::size_t n_line) const
+{
+	return _lines.at(n_line).second;
+}
+
+std::size_t Source_data::n_lines() const
+{
+	return _lines.size();
+}
---
+++ asm/source_data.hpp
@@ -0,0 +1,40 @@
+#ifndef LVASM_SOURCE_DATA_HPP_INCLUDED
+#define LVASM_SOURCE_DATA_HPP_INCLUDED
+
+#include <cstddef>
+#include <string>
+#include <vector>
+#include <deque>
+#include <experimental/filesystem>
+#include <utility>
+
+class Source_data {
+	using Source_line = std::pair<
+		const std::experimental::filesystem::path *,
+		std::string
+	>;
+
+	std::deque<std::experimental::filesystem::path> _source_paths;
+	std::vector<Source_line>                        _lines;
+
+	std::vector<Source_line> load_file(
+		const std::experimental::filesystem::path &filename
+	);
+
+public:
+	struct Position {
+		std::size_t line_n, col_n;
+	};
+	struct Range {
+		Position start, end;
+	};
+
+	Source_data(const std::string &root_filename);
+
+	std::string         get_range(const Range &range) const;
+	char                get_char(const Position &pos) const;
+	const std::string & get_line(std::size_t n_line) const;
+	std::size_t         n_lines() const;
+};
+
+#endif // LVASM_SOURCE_DATA_HPP_INCLUDED
