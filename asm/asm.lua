6784e4315870d1d37f77be1cb36abd8c30d3bca2
Christopher Leonard <chris.j.leonard@gmx.com>
2017-01-25 19:57:16 +0000

    asm: Added --bt 'backtrace' option

--- asm/asm.lua
+++ asm/asm.lua
@@ -15,9 +15,27 @@ function table.maxn(t)
 	return currentMax
 end
 
-local C_SourceFile = require("SourceFile")
-local C_ObjectFile = require("ObjectFile")
+g_doBacktrace = false
+for i, str in ipairs(arg) do
+	if str == "--bt" then
+		g_doBacktrace = true
+		table.remove(arg, i)
+		break
+	end
+end
 
-local sourceFile = C_SourceFile:New{rootFilename = arg[1]}
-local objectFile = C_ObjectFile:New{sourceFile = sourceFile}
-objectFile:WriteBinary(arg[2])
+xpcall(function()
+	local C_SourceFile = require("SourceFile")
+	local C_ObjectFile = require("ObjectFile")
+
+	local sourceFile = C_SourceFile:New{rootFilename = arg[1]}
+	local objectFile = C_ObjectFile:New{sourceFile = sourceFile}
+	objectFile:WriteBinary(arg[2])
+end,
+function(err)
+	if g_doBacktrace then
+		io.write(debug.traceback(err) .. "\n")
+	else
+		io.write(err .. "\n")
+	end
+end)
